1686f8418e38d30fb5c7fc1161b9487665dc640b
Luciara-Abreu <luciara.abreu@gmail.com>
2024-05-30 01:19:54 -0300

    feat: add page in the route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/home/Home';
+import Register from '../pages/register/Register';
+
 
 export const ListRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />} />   
+        <Route path="/register" element={<Register />} /> 
       </Routes>
     </BrowserRouter>
   );
