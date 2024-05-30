1d33ecabb0531b4b1b8a80c8c7fc704b52a14e5b
Luciara-Abreu <luciara.abreu@gmail.com>
2024-05-30 01:19:31 -0300

    feat: add page Register

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,56 @@
+import NavbarComponent from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+import RegisterForm from "../../components/registerForm/RegisterForm";
+import { ContainerRegister, BodyRegister } from "./styles";
+
+function Register() {
+   return (
+    <ContainerRegister>
+      <NavbarComponent />
+      <BodyRegister>
+
+        <RegisterForm onSubmit={function (data: { address: string; addressNumber: string; addressCep: string; fullName: string; phone: string; country: string; state: string; adressLine: string; }): void {
+          throw new Error("Function not implemented.");
+        } }/>
+
+      </BodyRegister>
+      <Footer />
+    </ContainerRegister>
+  )
+}
+
+export default Register;
+
+
+
+
+
+
+
+/**
+ * 
+ *   return (
+   <ContainerHome>
+     <NavbarComponent />
+     <BodyHome className="bodyHome">
+     <ContainerForm>
+     <form onSubmit={handleSubmit}>
+       <input type="text" value={numeroPL} onChange={e => setNumeroPL(e.target.value)} placeholder="Número do PL" />
+       <input type="text" value={dataPL} onChange={e => setDataPL(e.target.value)} placeholder="Data do PL" />
+       <button type="submit">Buscar</button>
+     </form>
+
+     <ul>
+       {propositions.map(proposition => (
+         <li key={proposition}>
+           {proposition}        
+         </li>
+       ))}
+     </ul>
+
+   </ContainerForm>
+     </BodyHome>
+     <Footer />
+   </ContainerHome>
+ )
+ */
---
+++ src/pages/register/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+
+export const ContainerRegister = styled.div`
+width: 100%;
+height: 100%;
+margin:0;
+padding:0;
+`
+
+
+export const BodyRegister = styled.div`
+width: 100%;
+height: 83.5vh;
+background: #f1ebeb;
+color: #000;
+
+p{
+  padding-top: 5%;
+  font-size: 30px;
+  color: #000;
+}
+`
+export const ContainerForm = styled.div`
+width: 100%;
+padding-top: 40px;
+`
