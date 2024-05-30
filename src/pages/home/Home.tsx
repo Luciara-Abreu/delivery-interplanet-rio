3bbb218fde3d8176207c0953877f4982827ee351
Luciara-Abreu <luciara.abreu@gmail.com>
2024-05-30 01:18:18 -0300

    feat: Add a Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,32 @@
 import NavbarComponent from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 import { ContainerHome, BodyHome, ContainerForm } from "./styles";
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 
 function Home() {
+  const history = useNavigate()
+
+  const handleClick = () => {
+    history('/register');
+  }
+
+
   return (
     <ContainerHome>
       <NavbarComponent />
+
       <BodyHome className="bodyHome">
+        <p>Bem-vindo ao Sistema de Delivery Interplanetário!</p>
         <ContainerForm> 
+          Faça seu cadastro & agilize o envio de sua mercadoria.
+
+          <Button variant="outline-dark" type="submit" onClick={handleClick}>CADASTRO</Button>     
+
         </ContainerForm>
       </BodyHome>
+
       <Footer />
     </ContainerHome>
   )
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -13,10 +13,12 @@ export const BodyHome = styled.div`
 width: 100%;
 height: 83.5vh;
 background: #f1ebeb;
-padding-top: 40px;
+color: #000;
 
-h1{
-  color: #0000;
+p{
+  padding-top: 5%;
+  font-size: 30px;
+  color: #000;
 }
 `
 export const ContainerForm = styled.div`
